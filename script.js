831f07ba9f661f92ae0c67c4aafc39b4673e3ca0
Sangam Arora <arorasam2109@gmail.com>
2023-07-15 19:18:53 +0530

    extra setting

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
 // Store the speech synthesis object
 const synth = window.speechSynthesis;
-
-
 const adviceText = document.getElementById('advice-text');
-
-
 const playButton = document.getElementById('play');
 const pauseButton = document.getElementById('pause');
 
 // SpeechSynthesisUtterance object to hold the current speech
 let currentSpeech = null;
-
 // Function to play the speech
 function playSpeech() {
     // Check if speech synthesis is supported
     if ('speechSynthesis' in window) {
         // Create a new SpeechSynthesisUtterance
         currentSpeech = new SpeechSynthesisUtterance(adviceText.textContent);
-
         // Play the speech
         synth.speak(currentSpeech);
     }
 }
-
 // Function to pause the speech
 function pauseSpeech() {
     // Check if speech synthesis is supported
@@ -31,12 +24,9 @@ function pauseSpeech() {
         synth.cancel();
     }
 }
-
 // Add event listeners to the play and pause buttons
 playButton.addEventListener('click', playSpeech);
 pauseButton.addEventListener('click', pauseSpeech);
-
-
 
 document.addEventListener("DOMContentLoaded", function () {
     const advice = document.getElementById("advice-id");
@@ -50,13 +40,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
 async function fetchquote() {
     var url = "https://type.fit/api/quotes";
-
-
     const response = await fetch(url);
     // console.log(typeof response);
-
     const allQuotes = await response.json();
-
     const indx = Math.floor(Math.random() * allQuotes.length);
     const quotePosition = document.getElementById("advice-text")
     const quote = allQuotes[indx].text;
